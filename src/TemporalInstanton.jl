16f44f86a39750248028655ade829e933a5442d3
Jonas Kersulis <kersulis@umich.edu>
2015-03-06 15:37:21 -0600

    finished mapping auxiliary angle variables

--- src/TemporalInstanton.jl
+++ src/TemporalInstanton.jl
@@ -68,30 +68,91 @@ function tmp_inst_b(n,T,G0,D)
     return sparse(b)
 end
 
-function tmp_inst_Qtheta(n,nr,T,tau,line,slack)
+function tmp_inst_Qtheta(n,nr,T,tau)
     """ Generate Q_theta in the temperature constraint
     of a temporal instanton problem instance.
     "line" has the form (i,k), where i and k refer to
     the endpoints of the chosen line.
     """
-    Qtheta = zeros((n+nr)*T,(n+nr)*T)
-    i,k = line
-    ei = zeros(n,1)
-    ei[i] = 1
-    ek = zeros(n,1)
-    ek[k] = 1
+    Qtheta = zeros((nr+n+1)*T,(nr+n+1)*T)
     
-    Q0 = (ei - ek)*(ei - ek)'
+    Qtheta[end-T+1:end,end-T+1:end] = eye(T)
+    return sparse(Qtheta)
+end
+
+# function tmp_inst_Qtheta(n,nr,T,tau,line,slack)
+#     """ Generate Q_theta in the temperature constraint
+#     of a temporal instanton problem instance.
+#     "line" has the form (i,k), where i and k refer to
+#     the endpoints of the chosen line.
+#     """
+#     Qtheta = zeros((n+nr)*T,(n+nr)*T)
+#     i,k = line
+#     ei = zeros(n,1)
+#     ei[i] = 1
+#     ek = zeros(n,1)
+#     ek[k] = 1
     
-    Q0 = Q0[:,setdiff(1:n,slack)]
-    Q0 = Q0[setdiff(1:n,slack),:]
+#     Q0 = (ei - ek)*(ei - ek)'
+    
+#     Q0 = Q0[:,setdiff(1:n,slack)]
+#     Q0 = Q0[setdiff(1:n,slack),:]
+    
+#     for t = 1:T
+#         start = nr + 1 + (nr+n)*(t-1)
+#         stop = start + n - 2
+#         Qtheta[start:stop,start:stop] = tau^(T-t)*Q0
+#     end
+#     return sparse(Qtheta)
+# end
+
+function tmp_inst_A_scale(n,Ridx,T,tau,slack,line)
+    """ Augment A with T additional rows relating
+    angle difference variables to angle variables.
+    
+    Returns a T-by-(n+nr+1)*T matrix that may be
+    concatenated with the output of temp_inst_A
+    
+    Arguments:    
+    * n is the number of nodes in the network
+    * Ridx is a vector indicating wind nodes
+    * T is the number of time steps
+    * tau is the thermal coefficient from IEEE 738
+    * slack is the index of the slack bus
+    * line is the pair (i,k) indicating the chosen
+    line
+    """
+    (i,k) = line
+    nr = length(Ridx)
+    
+    A = zeros(T,(nr+n+2)*T)
     
     for t = 1:T
-        start = nr + 1 + (nr+n)*(t-1)
-        stop = start + n - 2
-        Qtheta[start:stop,start:stop] = tau^(T-t)*Q0
+        i_pos = (nr+n+1)*(t-1) + n + i
+        k_pos = (nr+n+1)*(t-1) + n + k
+        coef = tau^((1/2)*(T-t))
+        A[t,i_pos] = -coef
+        A[t,k_pos] = coef
+        A[t,(n+nr+1)*T + t] = 1
     end
-    return sparse(Qtheta)
+    slack_cols = [(nr+n+1)*(t-1) + nr + slack for t in 1:T]
+    
+    # remove slack columns:
+    return sparse(A[:,setdiff(1:(n+nr+2)*T,slack_cols)])
+end
+
+function tmp_inst_pad_b(b,T)
+    """ Append T zeros to vector b
+    """
+    append!(b,zeros(T))
+end
+
+function tmp_isnt_pad_Q(Q,T)
+    """ Add T rows and T columns of zeros
+    to matrix Q
+    """
+    m,n = size(Q)
+    return [[Q zeros(m,T)]; zeros(T,n+T)]
 end
 
 function temporalInstanton(n,t,tau,Y,k,G0,D,line)
