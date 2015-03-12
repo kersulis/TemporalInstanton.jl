04b35ed56952d5702b6560d5a91a9beecb1e7320
Jonas Kersulis <kersulis@umich.edu>
2015-03-11 22:43:58 -0400

    solving the secular equation...

--- src/TrustRegionSubproblem.jl
+++ src/TrustRegionSubproblem.jl
@@ -1,6 +1,10 @@
 module TrustRegionSubproblem
 
-export min_norm, translate_quadratic, kernel_rotation, tr_translate, tr_kernel_rotate, tr_diag_rotate, tr_map_back, tr_trans_rotate
+export
+    min_norm, translate_quadratic, kernel_rotation, 
+    tr_translate, tr_kernel_rotate, tr_diag_rotate, 
+    tr_map_back, tr_trans_rotate, tr_check_diag,
+    tr_solve_secular, find_w
 
 # function min_norm(A::Array{Float64,2},b::Array{Float64,1})
 function min_norm(A,b)
@@ -113,6 +117,111 @@ function tr_trans_rotate(G_of_x,Q_of_x,A,b)
     K_of_w,Reigvec = tr_diag_rotate(J_of_z)
     
     return K_of_w,R_of_x,x_star,Rkernel,Reigvec
+end
+
+function find_w(mu,D,d,Qtheta)
+    # Evaluate secular equation
+    w = float([d[i]/(mu*Qtheta[i,i] - D[i,i]) for i in 1:length(d)])
+end
+
+function tr_check_diag(D,d,Qtheta,c)
+    eps = 1e-8
+    check = Bool[]
+    w_vals = Array(Vector{Float64},0)
+    mu_vals = FloatingPoint[]
+
+    # Check zero:
+    mu = 0
+    w = find_w(mu,D,d,Qtheta)
+    append!(check, [abs((w'*w)[1] - c^2) < eps])
+    push!(w_vals,w)
+    append!(mu_vals,[mu])
+
+    for i = 1:size(D,1)
+        mu = D[i,i]
+        w = find_w(mu,D,d,Qtheta)
+        append!(check, [abs((w'*w)[1] - c^2) < eps])
+        push!(w_vals,w)
+        append!(mu_vals,[mu])
+    end
+    return mu_vals,w_vals,check
+end
+
+function tr_solve_secular(D,d,Qtheta,c)
+    """ Solve the secular equation via binary search.
+    """
+    eps = 1e-8
+    solutions = Float64[]
+    poles = sort(diag(D*Qtheta))
+    # Each diagonal element is a pole.
+    for i in 1:length(poles)
+        
+        # Head left first:
+        high = poles[i]
+        if i == 1
+            low = high - abs(poles[i] - poles[i+1])
+        else
+            low = high - abs(poles[i] - poles[i-1])/2
+        end
+        
+        # Initialize mu:
+        mu = (high + low)/2
+        w = find_w(mu,D,d,Qtheta)
+        diff = (w'*w)[1] - c^2
+        diff_old = 0
+        stall = false
+        while abs(diff) > eps
+            if diff == diff_old
+                stall = true
+                break
+            end
+            if diff > 0
+                high = mu
+            else
+                low = mu
+            end
+            mu = (high + low)/2
+            w = find_w(mu,D,d,Qtheta)
+            diff_old = diff
+            diff = (w'*w)[1] - c^2
+        end
+        if !stall
+            push!(solutions,mu)
+        end
+        
+        # Now head right:
+        high = poles[i]
+        if i == length(poles)
+            low = high + abs(poles[i] - poles[i-1])
+        else
+            low = high + abs(poles[i] - poles[i+1])/2
+        end
+        
+        mu = (high + low)/2
+        w = find_w(mu,D,d,Qtheta)
+        diff = (w'*w)[1] - c^2
+        diff_old = 0
+        stall = false
+        while abs((w'*w)[1] - c^2) > eps
+            if diff == diff_old
+                stall = true
+                break
+            end
+            if diff > 0
+                high = mu
+            else
+                low = mu
+            end
+            mu = (high + low)/2
+            w = find_w(mu,D,d,Qtheta)
+            diff_old = diff
+            diff = (w'*w)[1] - c^2
+        end
+        if !stall
+            push!(solutions,mu)
+        end
+    end
+    return solutions
 end
 
 end
