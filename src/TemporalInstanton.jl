2201b1eef0b93d4c83baa8ff707a8a7c5d8ff311
Jonas Kersulis <kersulis@umich.edu>
2015-11-05 11:16:59 -0500

    only export functions that should be used

--- src/TemporalInstanton.jl
+++ src/TemporalInstanton.jl
@@ -1,31 +1,19 @@
 module TemporalInstanton
 export
     solve_instanton_qcqp, solve_temporal_instanton, LineParams,
-    ConductorParams, load_rts96_data, createY,
-    process_instanton_results, InstantonInputData, InstantonOutputData,
-    mat2tmpinst,
-
-    # temporary:
-    tmp_inst_Qobj,tmp_inst_A1,tmp_inst_b,tmp_inst_Qconstr,
-    return_conductor_params,return_thermal_constants,tmp_inst_A2,
-    kernel_rotation,
+    ConductorParams, load_rts96_data, InstantonInputData, InstantonOutputData, mat2tmpinst,
 
     # power flow:
     expand_renewable_vector,fixed_wind_A,fixed_wind_b,return_angles,
     return_angle_diffs,
-
-    # plot:
-    temperatureTrajectory
 
 include("DataLoad.jl")
 include("PowerFlow.jl")
 include("ThermalModel.jl")
 include("QCQPMatrixBuilding.jl")
 include("manipulations.jl")
-include("SolveSecular.jl")
 # include("plot.jl")
 include("solvetmpinst.jl")
-
 end
 
 @doc """
