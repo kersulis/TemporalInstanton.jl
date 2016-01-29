dc8b334379fcca6cdfcd58f39253c6b8579bbfe1
Jonas Kersulis <kersulis@umich.edu>
2016-01-29 11:06:27 -0500

    Julia translation of Jon's admittance matrix fcn

---
+++ src/getY.jl
@@ -0,0 +1,56 @@
+"""
+    (Y,Yf,Yt) = getY(lineOut,f,t,r,x,b,tap,ysh)
+This function builds admittance matrices.
+INPUTS:
+*  `lineOut`: [nline x 1, logical] which lines are out of service
+*  `f`: [nline x 1, double] from buses (range 1 to nbus)
+*  `t`: [nline x 1, double] to buses (range 1 to nbus)
+*  `r`: [nline x 1, double] series resistance (pu)
+*  `x`: [nline x 1, double] series reactance (pu)
+*  `b`: [nline x 1, double] line charging shunt susceptance (pu)
+*  `tap`: [nline x 1, double] complex tap ratio (pu)
+*  `ysh`: [nbus x 1, double] complex bus shunt admittance (pu)
+Matpower version
+for each branch, compute the elements of the branch admittance matrix where
+  | If |   | Yff  Yft |   | Vf |
+  |    | = |          | * |    |
+  | It |   | Ytf  Ytt |   | Vt |
+"""
+function getY(
+    lineOut::Vector{Bool},
+    f::Vector{Int64},
+    t::Vector{Int64},
+    r::Vector{Float64},
+    x::Vector{Float64},
+    b::Vector{Float64},
+    tap=fill(Complex(1.),length(f))::Vector{Complex{Float64}},
+    ysh=fill(0.im,length(unique([f;t])))::Vector{Complex{Float64}}
+    )
+    inService = find(!lineOut)
+    tap = tap[inService]
+    fis = f[inService]
+    tis = t[inService]
+
+    Ys = (1./(r + x*im))[inService]
+    Bc = b[inService]
+    Ytt = Ys + Bc*im/2
+    Yff = Ytt./(tap.*conj(tap))
+    Yft = -Ys./conj(tap)
+    Ytf = -Ys./tap
+
+    # build connection matrices
+    nbus,nline = length(ysh),length(lineOut)
+    # connection matrix for line and from buses
+    Cf = sparse(1:nline,f,ones(nline),nline,nbus)
+    # connection matrix for line and to buses
+    Ct = sparse(1:nline,t,ones(nline),nline,nbus)
+
+    # build Yf and Yt such that Yf*V is the vector of complex branch currents injected.
+    i = [inService;inService]
+    Yf = sparse(i,[fis;tis],[Yff;Yft],nline,nbus)
+    Yt = sparse(i,[fis;tis],[Ytf;Ytt],nline,nbus)
+
+    # build Ybus
+    Y = Cf'*Yf + Ct'*Yt + sparse(1:nbus,1:nbus,ysh,nbus,nbus)
+    return Y #,Yf,Yt
+end
