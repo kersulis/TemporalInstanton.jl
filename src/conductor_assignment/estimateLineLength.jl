d5ebe00a245f42a18bb12920f116650d6d4ca173
Jonas Kersulis <kersulis@umich.edu>
2016-02-23 15:01:25 -0500

    get line length estimation working

--- src/conductor_assignment/estimateLineLength.jl
+++ src/conductor_assignment/estimateLineLength.jl
@@ -19,28 +19,28 @@ Input
 Output
   `lengths` vector of line lengths [miles]
 """
-function estimateLineLength(System)
+function estimateLineLength(ps::PowerSystem)
     # Set default length
     default = 1e-4
 
     # Initialize output
-    lengths = zeros(length(System.Line),1)
+    lengths = zeros(length(ps.line))
 
-    for i = 1:length(System.Line)
-        if isempty(System.Line[i].conductorModel.R_unit)
+    for i = 1:length(ps.line)
+        if isnan(ps.line[i].conductorModel.R_unit)
             # Line has no conductor model, so set it to the default length
             lengths[i] = default
         else
             # Line has a conductor model, so estimate its length.
             # Find pu base for resistance for this line
-            rBase = System.Bus([System.Bus[:].num]==System.Line[i].from).vBase^2/System.Params.sBase
+            rBase = ps.bus[findfirst([b.num for b in ps.bus].==ps.line[i].from)].vBase^2/ps.params.sBase
 
             # Find resistance of line in ohms
-            rOhms = System.Line[i].R*rBase
+            rOhms = ps.line[i].R*rBase
 
             # Estimate line length in miles (mi = ohms/[ohms/m]/[m/mi])
-            lengths[i] = rOhms/(System.Line[i].conductorModel.R_unit/...
-                System.Line[i].conductorModel.bundle)/1609.34
+            lengths[i] = rOhms/(ps.line[i].conductorModel.R_unit/
+                ps.line[i].conductorModel.bundle)# /1609.34
         end
     end
 
