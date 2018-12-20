9e7f5598a66372e6f23adf432a48086525c07839
kersulis <kersulis@umich.edu>
2018-12-20 00:19:38 -0500

    temporarily add old code

--- src/TemporalInstanton.jl
+++ src/TemporalInstanton.jl
@@ -1,5 +1,38 @@
 module TemporalInstanton
 
 include("powersystem.jl")
+include("thermalmodel.jl")
 
 end # module
+
+# Old code:
+
+# export
+#     solve_instanton_qcqp, solve_temporal_instanton, LineParams,
+#     ConductorParams, load_rts96_data, InstantonInputData, InstantonOutputData, mat2tmpinst, process_instanton_results, permutecols,
+#
+#     # power flow:
+#     expand_renewable_vector,fixed_wind_A,fixed_wind_b,return_angles,
+#     return_angle_diffs,createY,
+#
+#     # collab:
+#     dict2type,ps2instantoninput,
+#
+#     testcase
+#
+# include("PowerSystem.jl")
+# include("ThermalModel.jl")
+# include("DataLoad.jl")
+# include("PowerFlow.jl")
+# include("QCQPMatrixBuilding.jl")
+# include("manipulations.jl")
+# include("PowerSystem.jl")
+# # include("plot.jl")
+# include("solvetmpinst.jl")
+# include("solvetmpinst-collab.jl")
+# end
+#
+# @doc """
+# Top-level module for performing temporal instanton analysis.
+# See individual functions in included files for documentation.
+# """ -> TemporalInstanton
