ca24a6527efd61ce49f50f44d827c9949c514823
Jonas Kersulis <kersulis@umich.edu>
2019-06-26 13:31:16 -0400

    Delete reference to non-existent plot file

--- src/TemporalInstanton.jl
+++ src/TemporalInstanton.jl
@@ -7,7 +7,6 @@ include("powerflow.jl")
 include("qcqpmatrices.jl")
 include("manipulations.jl")
 include("solvetmpinst.jl")
-include("plot.jl")
 
 export InstantonInput, InstantonOutput, LineParams, ConductorParams, build_instanton_input, conventional_to_renewable!, set_temperatures!, set_timing!, set_injections!, solve_instanton_qcqp, solve_temporal_instanton, process_instanton_results, return_conductor_params, temperature_trajectory,
 temperature_trajectories, steady_state_temps
